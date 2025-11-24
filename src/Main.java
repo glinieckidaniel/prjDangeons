ae323205a7fb3f20a94169bf971822ea7d41b2e5
glinieckidaniel <daniel.gliniecki@buonarorti.tn.it>
2025-11-24 11:12:57 +0100

    aggiornato

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        List<Personaggio> Lista;
+        Personaggio p1 = new Personaggio("Abdul",67,1,0,2.5,);
 
 
 
