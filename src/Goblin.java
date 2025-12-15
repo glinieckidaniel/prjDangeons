dfa712b4004655b2314114acf93e87530d1b2e86
glinieckidaniel <daniel.gliniecki@buonarorti.tn.it>
2025-12-15 11:08:46 +0100

    aggiornato

--- src/Goblin.java
+++ src/Goblin.java
@@ -1,2 +1,22 @@
-public class Goblin     {
+public class Goblin extends Nemici{
+    private int agilita;
+    public Goblin(String nome, int[] pos, int atk, int def, int hp, int maxHp, double range, boolean isBoss,int agilita) {
+        super(nome, pos, atk, def, hp, maxHp, range, isBoss);
+        this.agilita=agilita;
+    }
+    @Override
+    public boolean subisciDanno(int danno){
+        if(Math.random()*100+1<=agilita/2){
+            danno=0;
+        }
+        setHp(getHp()-danno);
+        return isVivo();
+    }
+    @Override
+    public int attacco(Personaggio nemico){
+        int danno=getAtk()*3+agilita-nemico.getDef()*2;
+        return danno;
+    }
+
 }
+
